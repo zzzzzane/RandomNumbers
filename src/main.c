cacc92deaa9c532ec90a7eee77448aba0b03b97f
Nate G. <iNate71@users.noreply.github.com>
2014-04-25 16:17:38 -0400

    Clean up and font change
    I changed the font type and adjusted the strings to account for this. Tell me what you think. If you don't like it, just change lines 91-98 to what you had, and change the FONT_KEY back to what you wanted.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
-//standard header
 #include "pebble.h"
-#include "iota.h"
+#include "buff.h"
 
 //app-specific data
 Window *window; //obvious window is obvious
@@ -14,8 +13,7 @@ static void fill_window(struct tm* tick_time) {
  	static char time_minutes[] = "00";
   	static char time_month[] = "00";
   	static char time_day[] = "00";
-	int r; //init variables for number of lines and random numbers
-	//can't init 'r' inside switch-statement due to restrictions of C; boo-hiss
+	int r; //init variable for rand numbers
 	
 	/*each time-component size*/
   	strftime(time_hours, sizeof("00"), "%I", tick_time);
@@ -25,36 +23,36 @@ static void fill_window(struct tm* tick_time) {
 	
   	srand(time(NULL)); //'NULL' tick event
 
-  	//loop prints 40 number lines
-  	for (int i = 1; i < 41; i++) {
+  	//loop prints 36 number lines
+  	for (int i = 1; i < 37; i++) {
 	  	switch(i)
 		{
-			case 1 : //i == 1
-		  	strcpy(full_string, time_hours); //hours to first row, first slot
+			case 1 :
+		  	strcpy(full_string, time_hours);
 			strcat(full_string, ".");
 		  	continue;
 			
-		  	case 5 : //i == 5
-		  	strcat(full_string, time_minutes); //minutes to first row, last slot
+		  	case 6 :
+		  	strcat(full_string, time_minutes);
 			strcat(full_string, "\n");
+		  	continue;
+			
+		  	case 31 :
+		  	strcat(full_string, time_month);
+			strcat(full_string, ".");
 		  	continue;
 			
 		  	case 36 :
-		  	strcat(full_string, time_month); //month to last row, last slot
-			strcat(full_string, ".");
-		  	continue;
-			
-		  	case 40 : //i == 40
-		  	strcat(full_string, time_day); //day to last row, last slot
+		  	strcat(full_string, time_day);
 			strcat(full_string, "\n");
 			continue;
 		  
-		  	default : //if 'i' isn't equal to one of the values above... do this instead!
-		  	r = rand() % 99 + 1; //random number, 1-99; this bypasses the whole check for zero
+		  	default :
+		  	r = rand() % 99 + 1; //random number, 1-99
 		  	if (r < 10)
 				strcat(full_string, "0");
-			strcat(full_string, itoa(r));
-			if ((i % 5) == 0)
+			strcat(full_string, buff(r));
+			if ((i % 6) == 0)
 				strcat(full_string, "\n");
 			else
 				strcat(full_string, ".");
@@ -63,13 +61,45 @@ static void fill_window(struct tm* tick_time) {
   	text_layer_set_text(time_layer, full_string); //set the text
 }
   
-/*called once per second*/
-static void handle_second_tick(struct tm* tick_time, TimeUnits units_changed) {
+/*called once per minute*/
+static void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed) {
 	
-	/*todo: make random numbers change once per 10 minutes, otherwise, change JUST the time&date*/
-	if ((tick_time->tm_sec % 10) == 0) //updates display once per 20 seconds
-		fill_window(tick_time);
+	static char full_string2[125];
+  	static char time_hours[] = "00";
+ 	static char time_minutes[] = "00";
+  	static char time_month[] = "00";
+  	static char time_day[] = "00";
+        const char *StringPtr;
+	
+	/*random numbers change once per 10 minutes*/ 
+	if ((tick_time->tm_min % 10) == 0) {
+    	fill_window(tick_time);
+	} else {
+  		/*handle 24 hour time setting */
+  		if (clock_is_24h_style())
+  			strftime(time_hours, sizeof("00"), "%H", tick_time);
+		else
+			strftime(time_hours, sizeof("00"), "%I", tick_time);
+	}
+  	strftime(time_minutes, sizeof("00"), "%M", tick_time);
+  	strftime(time_month, sizeof("00"), "%m", tick_time);
+  	strftime(time_day, sizeof("00"), "%d", tick_time);
+  
+  	/* replace the date and time only */
+    StringPtr = text_layer_get_text(time_layer);
+    strcpy(full_string2, StringPtr);
+    full_string2[0] = time_hours[0];
+    full_string2[1] = time_hours[1];
+    full_string2[15] = time_minutes[0];
+    full_string2[16] = time_minutes[1];
+    full_string2[105] = time_month[0];
+    full_string2[106] = time_month[1];
+    full_string2[105] = time_day[0];
+    full_string2[106] = time_day[1];
+	
+    text_layer_set_text(time_layer, full_string2); //set the text
 }
+
 
 /*handle the start-up of the app*/
 static void do_init(void) {
@@ -84,7 +114,7 @@ static void do_init(void) {
   	text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
   	text_layer_set_text_color(time_layer, GColorWhite);
   	text_layer_set_background_color(time_layer, GColorClear);
-  	text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+  	text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
 
   	/*ensures time is displayed immediately (will break if 'NULL' tick event accessed);
   	(this is why it's a good idea to have a separate routine to do the update itself)*/
@@ -92,14 +122,15 @@ static void do_init(void) {
   	struct tm *current_time = localtime(&now);
   	fill_window(current_time);
   	handle_minute_tick(current_time, MINUTE_UNIT);
-  	tick_timer_service_subscribe(SECOND_UNIT, &handle_second_tick);
+	tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
 
   	layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
 }
 
 /*deinits the layers*/
 static void do_deinit(void) {
-   
+	
+	tick_timer_service_unsubscribe();
 	text_layer_destroy(time_layer);
   	window_destroy(window);
 }
